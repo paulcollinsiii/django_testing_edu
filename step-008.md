941d6d33f0548528260e457c81ed95e91e3bf092
Paul Collins <paul.collins@brightscope.com>
2013-09-24 17:09:59 -0700

    Add tests for voting on polls and viewing results
    These are the basic basic tests, and need more to flesh out what
    actually happens on the page, but at least it's a starting point

--- step-008.md
+++ step-008.md
@@ -12,6 +12,7 @@ for that.
 Big Views Don't Cry
 ===================
 
+(read the above to the tune of Big Girls Don't Cry)
 Pages that are important, shouldn't suddenly die on us. The easiest
 smoke test in the world is to use the django client and just load the
 page. If it tosses an error, ruroh. We can get a bit fancier if we know
@@ -30,6 +31,27 @@ can be done with the test client as well. Just keep in mind that you'll
 make your test code more fragile the more knowledge of the underlying
 system you put in. Tests (ESPECIALLY unit tests) should continue working
 even if you completely change the underlying implementation of the
-function being tested.
+function being tested. It's a nice ideal and it's worth taking the time
+to shoot for as much as possible, but as you move up the stack in
+testing the less possible that seems to become.
 
-# TODO: Get some tests for actually voting on polls put together
+Things we haven't touched on:
+
+* Poll results...
+* Permissions
+
+This set of tests deals with poll results and submitting votes, but
+notice! The way I've written the tests assumes a formset is being used.
+That's the price you pay for higher level tests. They have a bit more
+knowledge of the underlying implmentation so when it changes you end up
+having to update more of your tests. DISABLING YOUR TESTS IS NOT
+UPDATING YOUR TESTS. No matter how tempting it might be to shut your
+failing tests up that way =)
+
+
+Tags to follow
+==============
+
+Tags 9 - end are fixing tests, then more tests, then fixing.
+Further explination is mainly done through the code. If it's strange
+looking please send me a PR!!
--- tests/functional/poll/test_views.py
+++ tests/functional/poll/test_views.py
@@ -25,9 +25,6 @@ class TestPollPages(object):
 
 
 class TestPollDataPages(TestCase):
-    def setUp(self):
-        self.client = client.Client()
-
     @staticmethod
     def get_polls():
         return [factories.PollFactory() for x in range(5)]
@@ -54,4 +51,85 @@ class TestPollDataPages(TestCase):
         url = reverse('poll:detail', kwargs={'pk': poll.pk})
         response = self.client.get(url)
         for answer in poll.answer_set.all().iterator():
+            assert_contains(response, answer.text)
+
+
+class TestPollVotePages(TestCase):
+    USER_PASSWORD = 'sekret!'
+
+    def setUp(self):
+        """
+        We want the poll reset between every test, thus this can't be in
+        a setupClass method
+        """
+
+        self.poll = factories.PollFactory()
+        self.url = reverse('poll:vote', kwargs={'pk': self.poll.pk})
+        self.user = factories.UserFactory()
+        self.user.set_password(self.USER_PASSWORD)
+        self.user.save()
+
+    def test_vote_requires_login(self):
+        assert_code(self.client.get(self.url), 302)
+
+    def test_vote_simple_view(self):
+        self.client.login(username=self.user.username, password=self.USER_PASSWORD)
+        assert_code(self.client.get(self.url), 200)
+
+    def test_vote_simple_view_answers_present(self):
+        self.client.login(username=self.user.username, password=self.USER_PASSWORD)
+        response = self.client.get(self.url)
+        for answer in self.poll.answer_set.all():
+            assert_contains(response, answer.text)
+
+    def test_vote_inactive_poll_redirect(self):
+        self.client.login(username=self.user.username, password=self.USER_PASSWORD)
+        self.poll.start = factories.YEYESTERDAY
+        self.poll.end = factories.YESTERDAY
+        self.poll.save()
+        assert_code(self.client.get(self.url), 302)
+
+    def test_vote_saves_to_db(self):
+        """
+        This is the first test we have that's fairly fragile
+        Changing the implementation of the view here could break this test
+        even if the view is still doing the right thing!
+
+        This makes it a bit harder to maintain, but that's also kind of
+        the price of functional testing
+        """
+
+        self.client.login(username=self.user.username, password=self.USER_PASSWORD)
+        answer = self.poll.answer_set.all()[0]
+        form_data = {
+            'form-TOTAL_FORMS': u'1',
+            'form-INITIAL_FORMS': u'1',
+            'form-MAX_NUM_FORMS': u'',
+            'id_form-0-num_votes': '1',
+            'id_form-0-answer_id': answer.pk}
+        response = self.client.post(self.url, data=form_data)
+        assert_code(response, 302)  # Successful vote posts redirect to poll results
+
+
+class TestPollResultsView(TestCase):
+    USER_PASSWORD = 'sekret!'
+
+    def setUp(self):
+        """
+        We want the poll reset between every test, thus this can't be in
+        a setupClass method
+        """
+
+        self.poll = factories.PollFactory()
+        self.url = reverse('poll:results', kwargs={'pk': self.poll.pk})
+        self.user = factories.UserFactory()
+        self.user.set_password(self.USER_PASSWORD)
+        self.user.save()
+
+    def test_simple_view(self):
+        assert_code(self.client.get(self.url), 200)
+
+    def test_answers_in_view(self):
+        response = self.client.get(self.url)
+        for answer in self.poll.answer_set.all():
             assert_contains(response, answer.text)
